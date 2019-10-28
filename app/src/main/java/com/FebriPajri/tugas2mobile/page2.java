2b45766deab7969e29ac6f1b1ec6c5511700f137
febripajri <febri100290@gmail.com>
2019-10-28 15:33:28 +0700

    first commit

---
+++ app/src/main/java/com/FebriPajri/tugas2mobile/page2.java
@@ -0,0 +1,30 @@
+package com.FebriPajri.tugas2mobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class page2 extends AppCompatActivity {
+TextView txtdata;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2);
+
+        txtdata = (TextView) findViewById(R.id.txtdata);
+        Intent datas = getIntent();
+        String npm = datas.getStringExtra("data");
+        String nama = datas.getStringExtra("datas");
+        txtdata.setText(nama + "\n"+npm);
+
+    }
+    @Override
+    public void onBackPressed(){
+        Intent back = new Intent(page2.this,MainActivity.class);
+        startActivity(back);
+        finish();
+
+    }
+}
